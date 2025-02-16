685b757ab594fbf8bad53678aa55e52be02e6d27
skondeti94 <123513933+skondeti94@users.noreply.github.com>
2025-02-16 12:03:19 -0500

    intial

---
+++ src/main/java/com/fullstack/GillsNpaws/GillsNpawsApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.GillsNpaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GillsNpawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GillsNpawsApplication.class, args);
+	}
+
+}
